00b51f4524ba39f0a61486a4c526527ecedbe0f3
Yubing325 <ybliang000@gmail.com>
2019-11-13 06:52:49 -0600

    add alertify, auth0 decoder and bootswatch

--- AppUI/src/app/_services/error.interceptor.ts
+++ AppUI/src/app/_services/error.interceptor.ts
@@ -11,7 +11,7 @@ export class ErrorInterceptor implements HttpInterceptor {
   ): import('rxjs').Observable<import('@angular/common/http').HttpEvent<any>> {
     return next.handle(req).pipe(
         catchError( error => {
-            if (error.status === 401){
+            if (error.status === 401) {
                 return throwError(error.statusText);
             }
             if (error instanceof HttpErrorResponse) {
@@ -22,7 +22,7 @@ export class ErrorInterceptor implements HttpInterceptor {
 
                 const serverError = error.error;
                 let modelStateErrors = '';
-                if (serverError.errors && typeof serverError.errors === 'object'){
+                if (serverError.errors && typeof serverError.errors === 'object') {
                     for (const key in serverError.errors) {
                         if (serverError.errors[key]) {
                             modelStateErrors += serverError.errors[key] + '\n';
--- AppUI/src/app/nav/nav.component.ts
+++ AppUI/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -9,28 +11,26 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next =>{
-      console.log('login successfully');
-    }, error =>{
-      console.log(error);
+      this.alertify.success('login successfully');
+    }, error => {
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
